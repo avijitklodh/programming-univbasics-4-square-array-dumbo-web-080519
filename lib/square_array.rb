433be9cc01f3d88456a4e59d249e0c769ff3e411
Avijit Lodh <avijitklodh@gmail.com>
2019-07-25 22:45:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,14 +1,10 @@
-
+# find the dumplicate keys
+# count the duplicate keys
+# delete the duplicate key
+# add the count as an elemnt to the duplicated key
 
 
 def square_array(array)
   # your code here
-  new_array =[]
-  counter = 0 
-  while counter <array.length do
-    new_element = array[counter]**2 
-    new_array << new_element
-    counter += 1 
-  end
-  return new_array
+
 end
